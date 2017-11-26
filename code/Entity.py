ff9cb373727afaff208e8d3a06cc30ac3c44b3a4
littlepig2013 <zhuzichen19960417@gmail.com>
2017-11-26 20:32:32 +0800

    WsRel and SignSim updated

--- code/Entity.py
+++ code/Entity.py
@@ -7,4 +7,9 @@ class Entity(object):
 	def __eq__(self, other):
 		return self.entityId == other.entityId and self.entityType == other.entityType
 
+	def __repr__(self):
+		return "Entity: entityType->" + str(self.entityType) + ", entityId->"+str(self.entityId)
+
+	def __str__(self): 
+		return self.__repr__()
 
--- code/Relation.py
+++ code/Relation.py
@@ -7,3 +7,9 @@ class Relation(object):
 
 	def __eq__(self, other):
 		return self.startEntity == other.startEntity and self.endEntity == other.endEntity
+
+	def __repr__(self):
+		return "Start Entity: " + str(self.startEntity) + "\n" + "End Entity: " + str(self.endEntity)
+
+	def __str__(self): 
+		return self.__repr__()
